8cc65ade8ac02933f69fda007e02c410942cf625
Anthony Kuske <anthonykuske@gmail.com>
2015-08-14 00:20:26 +0100

    Add release

--- version.php
+++ version.php
@@ -27,4 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'atto_mediadrop';
 $plugin->version = 2015081304;
 $plugin->requires = 2013111800; // MOODLE_26_STABLE
+$plugin->release = '1.0';
 $plugin->maturity = MATURITY_STABLE;
